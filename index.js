ac47cd8f42570639a785cefeca34c765e5641e91
Allen Shin <geeuho@gmail.com>
2019-12-09 14:45:12 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(people, adjective) {
+    let array = []
+    for (let i = 0; i < people.length; i++){
+        array.push(`Thank you, ${people[i]}, for the wonderful ${adjective} gift!`)
+    }
+    return array;
+}
+
+function countDown(number){
+    for (let i = number; i >= 0; i--){
+        console.log(i)
+    }
+}    

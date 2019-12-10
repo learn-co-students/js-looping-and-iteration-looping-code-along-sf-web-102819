faf227bd1b77297949b39568fe360f2ce32c09a8
Will Diep <widiep@gmail.com>
2019-12-10 10:05:43 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+let names = [ 'Lisa', 'Kaitlin', 'Jan' ]
+
+function writeCards(arr) {
+    let result = []
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i]
+        result.push(`Thank you, ${name}, for the wonderful surprise gift!`)
+    }
+    return result
+}
+
+// console.log(writeCards(names))
+
+function countDown(num) {
+
+    // while (num > 0) {
+    //     console.log(num)
+    //     num--
+    // }
+    // console.log(num)
+
+    for (let i = num; i >= 0; i--) {
+        console.log(i)
+    }
+}
+
+countDown(10)
